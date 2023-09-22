5628583bbe7ac062e4b7580395e0cc7099d0fdf9
pamalo9 <pamalo2002@gmail.com>
2023-09-22 14:14:42 +0200

    Change call_Api_au function, create get_data_meteo

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -6,47 +6,72 @@ import time
 
 # Define variables
 API_URL = "https://climate-api.open-meteo.com/v1/climate?"
+
 COORDINATES = {
     "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
     "London": {"latitude": 51.507351, "longitude": -0.127758},
     "Rio": {"latitude": -22.906847, "longitude": -43.172896},
 }
+
 VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean"
 
-
 # Create functions
-def call_API_aux(API):
-    #Call the api
-    r = requests.get(API_URL)
-    #Check the connections
+def call_API_aux(url):
+    r = requests.get(url)
+
+# Check the connections
     if r.status_code == 200:
-        print("Connected to the API")  #correct connection
+        print("Connection without errors") 
+        return r.json()  # correct connection
+
     else:
-        print("Number Error: " + str(r.status_code) + "\nType: " + r.text) #Error number and explanation
-    
-    #Check rate limit error and make cool off
+        print("Number Error: " + str(r.status_code) + "\nType: " + r.text)  # Error explanation
+
+# Check rate limit error and make cool off(Wait 3 seconds)
     for _ in range(3):
         if r.status_code == 429:
             print("ERROR: " + str(r.status_code) + " \nType: Max rate limit, you need to wait a bit ")
             time.sleep(10)
         continue
 
-def get_data_meteo_api(city):
-    return None
+# Obtain data from a city from the API
+def get_data_meteo_api(city, start_date, end_date, temporal_res):
+    coordinates = COORDINATES.get(city)
 
+    if coordinates:
+        lat = coordinates["latitude"]
+        long = coordinates["longitude"]
+        #New request to the API
+        url = f"{API_URL}latitude={lat}&longitude={long}&temporal_resolution={temporal_res}&variables={VARIABLES}&start_date={start_date}&end_date={end_date}"
+        return call_API_aux(url)
+    
+    else:
+        print("Coordinate error")
+        return None
+
+
+    
 
 def compute():
     return None
+
 
 def plot_results():
     return None
 
 
 def main():
-    call_API_aux(API_URL)
-    # raise NotImplementedError
-    
-    
+
+# raise NotImplementedError
+    temporal_res = "year"
+    start_date = "1950-01-01"
+    end_date = "2050-12-31"
+
+# Go through each city in the COORDINATES list and call the function to take each data.
+    for city in COORDINATES.keys():
+        city_data = get_data_meteo_api(city, start_date, end_date, temporal_res)
+        print("Dates of: " + city + " "+ str(city_data))
+
 
 if __name__ == "__main__":
     main()
