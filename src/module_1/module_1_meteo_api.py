774efa2182c07b8bebd6cfd0e21bda2427a530f8
pamalo9 <pamalo2002@gmail.com>
2023-09-23 19:36:01 +0200

    Create compute functions-change get_data-main code

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -3,6 +3,8 @@
 import requests
 import json
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Define variables
 API_URL = "https://climate-api.open-meteo.com/v1/climate?"
@@ -14,7 +16,7 @@ COORDINATES = {
 }
 
 VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean"
-
+MODELS = "CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S"
 # Create functions
 def call_API_aux(url):
     r = requests.get(url)
@@ -32,46 +34,71 @@ def call_API_aux(url):
         if r.status_code == 429:
             print("ERROR: " + str(r.status_code) + " \nType: Max rate limit, you need to wait a bit ")
             time.sleep(10)
+            r = requests.get(url)
         continue
+    return None
 
 # Obtain data from a city from the API
-def get_data_meteo_api(city, start_date, end_date, temporal_res):
+def get_data_meteo_api(city, start_date, end_date, temporal_resolution):
     coordinates = COORDINATES.get(city)
 
     if coordinates:
         lat = coordinates["latitude"]
         long = coordinates["longitude"]
+        
         #New request to the API
-        url = f"{API_URL}latitude={lat}&longitude={long}&temporal_resolution={temporal_res}&variables={VARIABLES}&start_date={start_date}&end_date={end_date}"
+        url = f"{API_URL}latitude={lat}&longitude={long}&start_date={start_date}&end_date={end_date}&models={MODELS}&temporal_resolution={temporal_resolution}&daily={VARIABLES}"
         return call_API_aux(url)
-    
     else:
         print("Coordinate error")
         return None
 
+def compute_average(data):
+   
+    return np.mean(data)
 
+def compute_dispersion(data):
     
-
-def compute():
-    return None
-
+    return np.std(data)
 
 def plot_results():
     return None
 
 
 def main():
-
+    
 # raise NotImplementedError
-    temporal_res = "year"
+    temporal_resolution = "year"
     start_date = "1950-01-01"
-    end_date = "2050-12-31"
+    end_date = "1951-12-31"
+    #models = "CMCC_CM2_VHR4"
 
 # Go through each city in the COORDINATES list and call the function to take each data.
     for city in COORDINATES.keys():
-        city_data = get_data_meteo_api(city, start_date, end_date, temporal_res)
-        print("Dates of: " + city + " "+ str(city_data))
+        city_data = get_data_meteo_api(city, start_date, end_date, temporal_resolution)
+        
+        #print(city_data)
+        print("Dates of: " + city + " " + str(city_data))
 
+        #I have errors in this part of the code because I can´t axcess to this variables (KeyError: 'temperature_2m_mean')
+        temperature_data = city_data['temperature_2m_mean']
+        precipitation_data = city_data["precipitation_sum"]
+        soil_moisture_data = city_data['soil_moisture_0_to_10cm_mean']
+            
+        temperature_average = compute_average(temperature_data)
+        temperature_dispersion = compute_dispersion(precipitation_data)
+            
+        precipitation_average = compute_average(precipitation_data)
+        precipitation_dispersion = compute_dispersion(precipitation_data)
+            
+        soil_moisture_average = compute_average(soil_moisture_data)
+        soil_moisture_dispersion = compute_dispersion(soil_moisture_data)
+            
+        print("Dates of:" + city + "\nTemperature - average: " + temperature_average + "- Dispersión: " + temperature_dispersion)
+        print("Precipitation - average:" + precipitation_average + "- Dispersión: " + precipitation_dispersion)
+        print("Soil moisture - average: " + soil_moisture_average + "- Dispersión: " + soil_moisture_dispersion)
+        print("\n")
+        plot_results(city, temperature_average, temperature_dispersion, precipitation_average, precipitation_dispersion, soil_moisture_average, soil_moisture_dispersion)
 
 if __name__ == "__main__":
     main()
